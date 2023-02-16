6a470a6be971cb9e012d046704d4fe4b30a9c5ff
1g2g <wonji126326@gmail.com>
2023-02-16 16:46:05 +0900

    #3 : event object

--- MOMENTOM/js/_event.js
+++ MOMENTOM/js/_event.js
@@ -1,3 +1,6 @@
+const $eventArea = document.getElementById("event");
+const $DOM = document.getElementById("_DOM");
+const $span = document.querySelector("#_DOM>span");
 function sayHi(user) {
   alert(`hi, ${user}`);
 }
@@ -17,3 +20,20 @@ btnList.forEach((btn) => {
     btn.style.backgroundColor = "white";
   });
 });
+
+//이벤트 객체
+const $firstname = document.getElementById("firstname");
+const $title = document.querySelector(".header");
+
+$firstname.addEventListener("click", () => {
+  const FIRSTNAME = prompt("Enter your firstname");
+  $title.textContent = `Hello, ${FIRSTNAME}`;
+  $DOM.insertBefore($title, $span);
+  localStorage.setItem("firstname", FIRSTNAME);
+});
+const setName = () => {
+  const FIRSTNAME = localStorage.getItem("firstname");
+  $title.textContent =
+    FIRSTNAME === null ? "Hello,anonymous" : `Hello, ${FIRSTNAME}`;
+};
+setName();
